30b6e840fcd02955cd66e2f54378d1ccd32df8f9
bremea <bremeagames@gmail.com>
2025-07-29 15:51:13 -0400

    proper 404 page

---
+++ apps/website/src/routes/(app)/[...path]/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Link } from '@minemaker/ui';
+</script>
+
+<main class="flex w-full flex-col items-center justify-center space-y-8 py-24">
+	<h1 class="text-6xl font-bold">404</h1>
+	<p>This page couldn't be found!</p>
+	<p>A little lost? Head back <Link href="/">home...</Link></p>
+</main>
