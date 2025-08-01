adc97c8d5ab339fc1519951a32068db7ac68c68e
bremea <bremeagames@gmail.com>
2025-08-01 01:09:11 -0400

    update game schemas to support nested objects

--- libs/db/src/models/game.ts
+++ libs/db/src/models/game.ts
@@ -1,8 +1,8 @@
 import { pool } from '../connection';
 import { InternalApiError, Nullable } from '../utils';
 import { t } from 'elysia';
-import { getUserByAccountId, User, UserSchema } from './user';
-import { TString } from '@sinclair/typebox';
+import { getUserByAccountId, parseProfileFromUser, Profile, ProfileSchema } from './user';
+import { BuildSchema } from './build';
 
 export enum GameFlags {
 	None = 0,
@@ -12,57 +12,50 @@ export enum GameFlags {
 	All = ~(~0 << 3) // 111
 }
 
-export const GameSchemaWithOwner = t.Object({
+export const GameSchema = t.Object({
 	id: t.String(),
-	owner: UserSchema,
+	owner: ProfileSchema,
 	name: t.String(),
 	thumbnail: Nullable(t.String()),
 	description: t.String(),
-	currentBuildId: Nullable(t.String()),
+	currentBuild: Nullable(t.Object({ id: t.String() })),
 	discoverable: t.Boolean(),
 	lastUpdated: t.Date(),
-	flags: t.Number({ minimum: GameFlags.None, maximum: GameFlags.All }),
+	flags: t.Enum(GameFlags),
 	tags: t.Array(t.String()),
 	online: t.Number()
 });
 
-export const GameSchemaWithoutOwner = t.Object({
-	id: t.String(),
-	owner: t.String(),
-	name: t.String(),
-	thumbnail: Nullable(t.String()),
-	description: t.String(),
-	currentBuildId: Nullable(t.String()),
-	discoverable: t.Boolean(),
-	lastUpdated: t.Date(),
-	flags: t.Number({ minimum: GameFlags.None, maximum: GameFlags.All }),
-	tags: t.Array(t.String()),
-	online: t.Number()
-});
+export type Game = typeof GameSchema.static;
 
-export type GameWithOwner = typeof GameSchemaWithOwner.static;
-export type GameWithoutOwner = typeof GameSchemaWithoutOwner.static;
-export type Game = GameWithoutOwner | GameWithOwner;
-
-function parseDatabaseGame(data: any, owner: User | undefined = undefined): Game {
+export function parseDatabaseGame(data: any): Game {
 	return {
-		id: data.id,
-		owner: owner ?? data.owner,
-		name: data.name,
-		thumbnail: data.thumbnail ?? 'e2300692-29ef-4ad4-c815-a759c59a8c00', // default thumbnail image id
-		description: data.description,
-		currentBuildId: data.current_build,
-		discoverable: data.discoverable,
-		lastUpdated: data.last_updated,
-		flags: parseInt(data.flags),
-		tags: data.tags ?? [],
-		online: data.online ?? 0
+		...parseDatabaseGameExcludeOwner(data),
+		owner: parseProfileFromUser(data.owner)
 	};
 }
 
-export async function getGameById(id: string, resolveOwner: boolean = true): Promise<Game> {
+export function parseDatabaseGameExcludeOwner(data: any): Omit<Game, 'owner'> {
+	return {
+		id: data.id.toString(),
+		name: data['name'],
+		thumbnail: data['thumbnail'] ?? 'e2300692-29ef-4ad4-c815-a759c59a8c00', // default thumbnail image id
+		description: data['description'],
+		currentBuild: data['current_build'] ? { id: data['current_build'] } : null,
+		discoverable: data['discoverable'],
+		lastUpdated:
+			typeof data['last_updated'] == 'string'
+				? new Date(data['last_updated'])
+				: data['last_updated'],
+		flags: parseInt(data['flags']),
+		tags: data['tags'] ?? [],
+		online: data['online'] ?? 0
+	};
+}
+
+export async function getGameById(id: string): Promise<Game> {
 	const res = await pool.query({
-		text: `SELECT * FROM games WHERE id = $1`,
+		text: `SELECT g.*, jsonb_build_object( 'account', to_jsonb(a), 'player', to_jsonb(p) ) AS owner FROM games g JOIN accounts a ON a.id = g.owner LEFT JOIN players p ON p.uuid = a.mc_uuid WHERE g.id = $1`,
 		values: [id]
 	});
 
@@ -70,35 +63,40 @@ export async function getGameById(id: string, resolveOwner: boolean = true): Pro
 		throw new InternalApiError(400, `No game exists with id ${id}`);
 	}
 
-	if (resolveOwner) {
-		return parseDatabaseGame(res.rows[0], await getUserByAccountId(res.rows[0].owner));
-	} else {
-		return parseDatabaseGame(res.rows[0]);
-	}
+	return parseDatabaseGame(res.rows[0]);
 }
 
 export async function createGame(
 	id: string,
 	owner: string,
 	name: string
-): Promise<GameWithoutOwner> {
+): Promise<Omit<Game, 'owner'>> {
 	const res = await pool.query({
 		text: `INSERT INTO games (id, owner, name) VALUES($1, $2, $3) RETURNING *`,
 		values: [id, owner, name]
 	});
 
-	return parseDatabaseGame(res.rows[0]) as GameWithoutOwner;
+	return parseDatabaseGameExcludeOwner(res.rows[0]);
 }
 
 export async function getUserGamesByCreationDate(
 	id: string,
 	start?: number,
 	limit?: number
-): Promise<GameWithoutOwner[]> {
+): Promise<Omit<Game, 'owner'>[]> {
 	const res = await pool.query({
 		text: `SELECT * FROM games WHERE owner = $1 ORDER BY last_updated DESC LIMIT $2 OFFSET $3`,
 		values: [id, limit ?? 25, start ?? 0]
 	});
 
-	return res.rows.map((e) => parseDatabaseGame(e) as GameWithoutOwner);
+	return res.rows.map((e) => parseDatabaseGameExcludeOwner(e));
+}
+
+export async function checkGameOwner(gameId: string, accountId: string): Promise<boolean> {
+		const res = await pool.query({
+		text: `SELECT 1 FROM games WHERE id = $1 AND owner = $2 LIMIT 1`,
+		values: [gameId, accountId]
+	});
+
+	return res.rows.length > 0;
 }
