49a05860e88d98d430ff386d6ec4198b63f9cf21
bremea <bremeagames@gmail.com>
2025-08-01 01:04:06 -0400

    update 404 to use minecraft font

--- apps/website/src/routes/(app)/[...path]/+error.svelte
+++ apps/website/src/routes/(app)/[...path]/+error.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { Link } from '@minemaker/ui';
+	import { page } from '$app/state';
 </script>
 
 <main class="flex w-full flex-col items-center justify-center space-y-8 py-24">
-	<h1 class="text-6xl font-bold">404</h1>
-	<p>This page couldn't be found!</p>
-	<p>A little lost? Head back <Link href="/">home...</Link></p>
+	<h1 class="font-minecraft text-8xl">{page.status}</h1>
+	{#if page.status == 404}
+		<p>This page couldn't be found!</p>
+	{/if}
+	<p>Head back <Link href="/">home...</Link></p>
 </main>
