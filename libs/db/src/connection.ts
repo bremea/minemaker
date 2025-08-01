9f72156d50d583a17ccee67337ab582434d85b27
bremea <bremeagames@gmail.com>
2025-08-01 01:06:05 -0400

    use json parser with bigint support when parsing json/jsonb

--- libs/db/src/connection.ts
+++ libs/db/src/connection.ts
@@ -1,5 +1,17 @@
 import { Pool } from 'pg';
 import 'dotenv/config';
+import types from 'pg-types';
+import JSONbig from 'json-bigint';
+
+const JSON_OID = 114;
+const JSONB_OID = 3802;
+
+types.setTypeParser(JSON_OID, (val) => jsonParse(val));
+types.setTypeParser(JSONB_OID, (val) => jsonParse(val));
+
+function jsonParse(input: string): any {
+	return JSONbig.parse(input);
+}
 
 export const pool = new Pool({
 	host: process.env.DB_HOST!,
