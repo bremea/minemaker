4641532e0665719e45ffc13522cc02e36ebc2954
bremea <bremeagames@gmail.com>
2025-08-01 01:10:29 -0400

    add build export

--- libs/ui/src/lib/components/misc/loader.svelte
+++ libs/ui/src/lib/components/misc/loader.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
-	import FluentSpinnerIos20Filled from '~icons/fluent/spinner-ios-20-filled';
+	import SvgSpinnersBlocksShuffle3 from '~icons/svg-spinners/blocks-shuffle-3';
+
+	type LoaderSizeOptions = 'sm' | 'md' | 'lg' | 'xl';
+	const sizeClasses: { [K in LoaderSizeOptions]: string } = {
+		sm: 'size-6',
+		md: 'size-8',
+		lg: 'size-12',
+		xl: 'size-16'
+	};
+
+	let { size = 'lg' }: { size?: LoaderSizeOptions } = $props();
 </script>
 
 <div class="flex h-full items-center justify-center py-24">
-	<FluentSpinnerIos20Filled class={`h-12 w-12 animate-spin opacity-70`} />
+	<SvgSpinnersBlocksShuffle3 class={`${sizeClasses[size]} opacity-70`} />
 </div>
---
+++ libs/ui/src/lib/components/navigation/links/oauth/index.ts
@@ -0,0 +1,3 @@
+export { default as MicrosoftLoginButton } from './microsoftLoginButton.svelte';
+export { default as DiscordLoginButton } from './discordLoginButton.svelte';
+export { default as OAuthButton } from './oauthButton.svelte';
