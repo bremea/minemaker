5d198a238f80d2aa98fb5c85a66e72a49a417b72
bremea <bremeagames@gmail.com>
2025-08-01 20:52:06 -0400

    add version to bottom of auth screens on studio

--- apps/studio/src/routes/(auth)/+layout.svelte
+++ apps/studio/src/routes/(auth)/+layout.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
 	let { children } = $props();
+	import { getVersion, getName } from '@tauri-apps/api/app';
+	import { onMount } from 'svelte';
+
+	let version = $state('');
+
+	onMount(async () => {
+		version = `${await getName()} v${await getVersion()}`
+	})
 </script>
 
 <main class="min-h-screen w-full bg-[url(/cherrybg.png)] bg-cover bg-center bg-no-repeat">
 	<div
 		class="backdrop-blur-xs relative flex min-h-screen w-full flex-col items-center justify-center space-y-4 p-12"
 	>
-		<a href="/" class="flex w-max transition-all hover:scale-105 focus:scale-105 active:scale-95">
-			<img src="/studiologo.png" alt="Minemaker Studio Logo" class="h-24 drop-shadow-md" />
-		</a>
+		<img
+			src="/studiologo.png"
+			alt="Minemaker Studio Logo"
+			class="h-24 drop-shadow-md select-none pointer-events-none"
+		/>
 		{@render children()}
+		<p class="absolute bottom-2 text-sm">{version}</p>
 	</div>
 </main>
