d07f920272a825848b78bd720a56b6905ec152e5
bremea <bremeagames@gmail.com>
2025-08-03 22:40:21 -0400

    move error page to a component

---
+++ apps/website/src/lib/components/errorPage.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Link } from '@minemaker/ui';
+	import { page } from '$app/state';
+</script>
+
+<main class="flex w-full flex-col items-center justify-center space-y-8 py-24">
+	<h1 class="font-minecraft text-8xl">{page.status}</h1>
+	{#if page.status == 404}
+		<p>This page couldn't be found!</p>
+	{/if}
+	<p>Head back <Link href="/">home...</Link></p>
+</main>
