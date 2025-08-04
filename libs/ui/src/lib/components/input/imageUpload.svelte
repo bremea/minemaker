8166a17da1c7c1db846c78dbc28c56031b86ef41
bremea <bremeagames@gmail.com>
2025-08-03 22:43:03 -0400

    update imageUpload to only accept first file

--- libs/ui/src/lib/components/input/imageUpload.svelte
+++ libs/ui/src/lib/components/input/imageUpload.svelte
@@ -2,16 +2,20 @@
 	import type { HTMLInputAttributes } from 'svelte/elements';
 	import FluentImageAdd24Filled from '~icons/fluent/image-add-24-filled';
 
+	let files: FileList | undefined = $state();
+
 	export interface ImageUploadProps extends HTMLInputAttributes {
 		class?: string;
-		files?: FileList;
+		img?: File;
 	}
 
-	function dropHandler(ev: DragEvent) {
+	let { children, class: className, id, img = $bindable(), ...others }: ImageUploadProps = $props();
+
+	async function dropHandler(ev: DragEvent) {
 		ev.preventDefault();
 
 		if (ev.dataTransfer?.files) {
-			files = ev.dataTransfer?.files;
+			files = ev.dataTransfer.files;
 		}
 	}
 
@@ -19,26 +23,31 @@
 		ev.preventDefault();
 	}
 
-	let {
-		children,
-		class: className,
-		id,
-		files = $bindable(),
-		...others
-	}: ImageUploadProps = $props();
+	$effect(() => {
+		if (files && files.length > 0) {
+			img = files[0];
+		}
+	});
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 <div class={`group relative ${className}`}>
 	{@render children?.()}
 	<div
-		class="absolute top-0 left-0 z-10 h-full w-full bg-black/75 opacity-0 transition-all group-hover:opacity-100"
+		class="absolute left-0 top-0 z-10 h-full w-full bg-black/75 opacity-0 transition-all group-hover:opacity-100"
 		ondrop={dropHandler}
 		ondragover={dragOverHandler}
 	>
 		<label for="file" class="flex h-full w-full cursor-pointer items-center justify-center">
 			<FluentImageAdd24Filled class="size-16" />
 		</label>
-		<input id="file" type="file" accept="image/png,image/jpg" class="hidden" bind:files />
+		<input
+			id="file"
+			type="file"
+			accept="image/png,image/jpg,image/gif,image/webp"
+			class="hidden"
+			bind:files
+			{...others}
+		/>
 	</div>
 </div>
